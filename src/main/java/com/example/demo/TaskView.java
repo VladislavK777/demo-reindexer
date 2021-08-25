950c199a5d652a7b60dfe29db2bcb7905ca74110
klochkov vladislav <v.a.klochkov@rt.ru>
2021-08-25 14:58:26 +0300

    Commit

---
+++ src/main/java/com/example/demo/TaskView.java
@@ -0,0 +1,32 @@
+package com.example.demo;
+
+import ru.rt.restream.reindexer.annotations.Reindex;
+
+public class TaskView {
+    @Reindex(name = "id", isPrimaryKey = true)
+    private Integer id;
+
+    @Reindex(name = "name")
+    private String name;
+
+    public TaskView(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
