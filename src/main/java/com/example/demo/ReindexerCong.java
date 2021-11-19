fcd82ba9922bf8ebc5d3e6be2a966dd5dd07270a
klochkov vladislav <v.a.klochkov@rt.ru>
2021-11-19 09:43:49 +0300

    reindexer close connection

--- src/main/java/com/example/demo/ReindexerCong.java
+++ src/main/java/com/example/demo/ReindexerCong.java
@@ -2,9 +2,11 @@ package com.example.demo;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import ru.rt.restream.reindexer.Namespace;
 import ru.rt.restream.reindexer.Reindexer;
 
 import static java.time.Duration.ofSeconds;
+import static ru.rt.restream.reindexer.NamespaceOptions.defaultOptions;
 import static ru.rt.restream.reindexer.ReindexerConfiguration.builder;
 
 @Configuration
@@ -13,9 +15,14 @@ public class ReindexerCong {
     @Bean(destroyMethod = "close")
     public Reindexer reindexer() {
         return builder()
-                .url("cproto://127.0.0.1:6534/task_manager")
+                .url("cproto://127.0.0.1:6534/test_db")
                 .connectionPoolSize(1)
                 .requestTimeout(ofSeconds(5L))
                 .getReindexer();
+    }
+
+    @Bean
+    public Namespace<TaskView> namespace() {
+        return reindexer().openNamespace("test_view", defaultOptions(), TaskView.class);
     }
 }
--- src/main/java/com/example/demo/Service.java
+++ src/main/java/com/example/demo/Service.java
@@ -4,31 +4,29 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import ru.rt.restream.reindexer.Namespace;
 import ru.rt.restream.reindexer.Reindexer;
-
-import static ru.rt.restream.reindexer.NamespaceOptions.defaultOptions;
 
 @Component
 @EnableScheduling
 public class Service {
-    private final Reindexer db;
+    private final Namespace<TaskView> namespace;
     int restart = 0;
 
     @Autowired
-    public Service(Reindexer reindexer) {
-        this.db = reindexer;
+    public Service(Namespace<TaskView> namespace) {
+        this.namespace = namespace;
     }
 
     @Scheduled(fixedDelayString = "60000")
     public void start() {
-        db.openNamespace("test_view", defaultOptions(), TaskView.class);
         int local = 0;
         try {
             for (int i = restart; i < 30000; i++) {
                 Thread.sleep(1000);
-                db.upsert("test_view", new TaskView(i, "Task_" + i));
+                namespace.upsert(new TaskView(i, "Task_" + i));
                 System.out.println(i);
-                local = i;
+                local = i + 1;
             }
         } catch (Exception e) {
             System.out.println("exception i = " + local);
